77b72095bce6eab8f9cb6e20e68302e32bed21da
Jared K <jarod137@nmsu.edu>
2023-03-03 17:34:15 -0700

    added keyboard reader funstionality and "player"

--- LegendOfNine/src/edu/nmsu/cs/legendofnine/GamePanel.java
+++ LegendOfNine/src/edu/nmsu/cs/legendofnine/GamePanel.java
@@ -3,6 +3,8 @@ package edu.nmsu.cs.legendofnine;
 import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 /*
  * Primary settings for the game panel window. Includes tile size and size of the game window.
@@ -20,11 +22,19 @@ public class GamePanel extends JPanel implements Runnable{
 	
 	final int screenWidth = tileSize * maxScreenCol;  // 1024 pixels
 	final int screenHeight = tileSize * maxScreenRow; // 768 pixels
+
+	// FPS
+	int FPS = 60;
 	
 	
-	
+	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
 	
+
+	// Set player's default position
+	int playerX = 100;
+	int playerY = 100;
+	int playerSpeed = 4;
 	
 	
 	/**
@@ -34,9 +44,14 @@ public class GamePanel extends JPanel implements Runnable{
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true); // Drawing done in offscreen painting buffer
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
 	} // end constructor
 	
-	
+	public void startGameThread() {
+		gameThread = new Thread(this);
+		gameThread.start(); 
+	} // end startGameThread
 	
 	/**
 	 * Run method for game
@@ -44,13 +59,62 @@ public class GamePanel extends JPanel implements Runnable{
 	@Override
 	public void run() {
 		//
-	} // end run
+	    // end run
 	
-	
-	
-	public void startGameThread() {
-		gameThread = new Thread(this);
-		gameThread.start();
-	} // end startGameThread
+		double drawInterval = 1000000000/FPS; // 0.016666 seconds
+		double nextDrawTime = System.nanoTime() + drawInterval;
+
+		while(gameThread != null) {
+
+			update();
+
+			repaint();
+
+			try {
+				double remainingTime = nextDrawTime - System.nanoTime();
+				remainingTime = remainingTime/1000000;
+
+				if (remainingTime < 0) {
+					remainingTime = 0;
+				}
+
+				Thread.sleep((long) remainingTime);
+
+				nextDrawTime += drawInterval;
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+	public void update() {
+		
+		if(keyH.upPressed == true) {
+			playerY -= playerSpeed;
+		}
+		if(keyH.downPressed == true) {
+			playerY += playerSpeed;
+		}
+		if(keyH.leftPressed == true) {
+			playerX -= playerSpeed;
+		}
+		if(keyH.rightPressed == true) {
+			playerX += playerSpeed;
+		}
+	}
+
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+
+		Graphics2D g2 = (Graphics2D)g;
+
+		g2.setColor(Color.white);
+
+		g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+		g2.dispose();
+	}
 
 } // end GamePanel
