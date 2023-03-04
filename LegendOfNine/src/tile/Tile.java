83ff807da9f04ef72cb565188348081242742943
rhollomon <rubyh2@nmsu.edu>
2023-03-04 14:58:43 -0600

    Added Tile class
    Contains variables for an individual tile

---
+++ LegendOfNine/src/tile/Tile.java
@@ -0,0 +1,13 @@
+package tile;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Class to represent a single tile
+ */
+public class Tile {
+	
+	public BufferedImage image;
+	public boolean collision = false;
+
+} // end Tile class
