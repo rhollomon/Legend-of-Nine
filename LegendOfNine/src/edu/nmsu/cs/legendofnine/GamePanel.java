e8fc220906a9c14acde33c8132a23dfc64c3b7fe
rhollomon <rubyh2@nmsu.edu>
2023-03-03 22:44:42 -0600

    Updated GamePanel to support sprite animation

--- LegendOfNine/src/edu/nmsu/cs/legendofnine/GamePanel.java
+++ LegendOfNine/src/edu/nmsu/cs/legendofnine/GamePanel.java
@@ -3,6 +3,7 @@ package edu.nmsu.cs.legendofnine;
 import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JPanel;
+import entity.Player;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
@@ -16,19 +17,20 @@ public class GamePanel extends JPanel implements Runnable{
 	// Screen settings
 	final int originalTileSize = 16; // 16 by 16 tile. FIXME when graphics are updated.
 	final int scale = 4;                            // Scales up pixel art by 4x
-	final int tileSize = originalTileSize * scale;  //Actual size of tile is 64x64.
+	public final int tileSize = originalTileSize * scale;  //Actual size of tile is 64x64.
 	final int maxScreenCol = 16;   // Number of tiles that fit on the game screen horizontally
 	final int maxScreenRow = 12;   // Number of tiles that fit on the game screen vertically
 	
 	final int screenWidth = tileSize * maxScreenCol;  // 1024 pixels
 	final int screenHeight = tileSize * maxScreenRow; // 768 pixels
 
-	// FPS
+	// Frames per second
 	int FPS = 60;
 	
 	
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
+	Player player = new Player(this, keyH); // Make new player from Player class
 	
 
 	// Set player's default position
@@ -48,18 +50,20 @@ public class GamePanel extends JPanel implements Runnable{
 		this.setFocusable(true);
 	} // end constructor
 	
+	
+	
 	public void startGameThread() {
 		gameThread = new Thread(this);
 		gameThread.start(); 
 	} // end startGameThread
+	
+	
 	
 	/**
 	 * Run method for game
 	 */
 	@Override
 	public void run() {
-		//
-	    // end run
 	
 		double drawInterval = 1000000000/FPS; // 0.016666 seconds
 		double nextDrawTime = System.nanoTime() + drawInterval;
@@ -87,34 +91,33 @@ public class GamePanel extends JPanel implements Runnable{
 			}
 		}
 
-	}
+	} // end run
+	
+	
+	
+	/**
+	 * Update player position
+	 */
 	public void update() {
 		
-		if(keyH.upPressed == true) {
-			playerY -= playerSpeed;
-		}
-		if(keyH.downPressed == true) {
-			playerY += playerSpeed;
-		}
-		if(keyH.leftPressed == true) {
-			playerX -= playerSpeed;
-		}
-		if(keyH.rightPressed == true) {
-			playerX += playerSpeed;
-		}
-	}
+		player.update();
+		
+	} // end update
 
+	
+	
+	/**
+	 * Display player sprite
+	 */
 	public void paintComponent(Graphics g) {
 		
 		super.paintComponent(g);
 
 		Graphics2D g2 = (Graphics2D)g;
-
-		g2.setColor(Color.white);
-
-		g2.fillRect(playerX, playerY, tileSize, tileSize);
+		
+		player.draw(g2);
 
 		g2.dispose();
-	}
+	} // end paintComponent
 
 } // end GamePanel
