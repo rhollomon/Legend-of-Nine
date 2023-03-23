e59067b56e388ffa5086c5c8753ed499ab9386b6
brockmiddleton <brockmiddleton4@gmail.com>
2023-03-23 16:41:22 -0600

    Update SuperObject class with Object Collision attributes

--- LegendOfNine/src/object/SuperObject.java
+++ LegendOfNine/src/object/SuperObject.java
@@ -1,6 +1,7 @@
 package object;
 
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 import edu.nmsu.cs.legendofnine.GamePanel;
@@ -11,6 +12,10 @@ public class SuperObject {
 	public String name;
 	public boolean collision = false;
 	public int worldX, worldY;
+
+	public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
+	public int solidAreaDefaultX = 0;
+	public int solidAreaDefaultY = 0;
 
 	public void draw(Graphics2D g2, GamePanel gp) {
 		
