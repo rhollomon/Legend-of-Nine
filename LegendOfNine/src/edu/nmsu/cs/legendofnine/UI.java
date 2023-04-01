2ddeeb495d074ba7c2c1bb51a8367ad1b4873e13
brockmiddleton <brockmiddleton4@gmail.com>
2023-03-31 18:57:30 -0600

    Update UI with titleState
    UI now has drawTitleScreen method which draws the title screen, including mouse sprite and selection buttons.

--- LegendOfNine/src/edu/nmsu/cs/legendofnine/UI.java
+++ LegendOfNine/src/edu/nmsu/cs/legendofnine/UI.java
@@ -28,6 +28,7 @@ public class UI {
 	int messageCounter = 0;
 	public boolean gameFinished = false;
 	public String currentDialogue = "";
+	public int commandNum = 0;
 	
 	double playTime;
 	DecimalFormat dFormat = new DecimalFormat("#0.00");
@@ -87,8 +88,10 @@ public class UI {
 	public void draw(Graphics2D g2) {
 		this.g2 = g2;
 		
-		
-		
+		// If we are in TITLE state
+		if (gp.gameState == gp.titleState) {
+			drawTitleScreen();
+		}
 		
 		if(gp.gameState == gp.playState) {
 			if(gameFinished) {
@@ -180,8 +183,57 @@ public class UI {
 	} // end draw
 	
 	
-	
-	
+	/**
+	 * Draw Title screen when in gp.titleState
+	 */
+	public void drawTitleScreen() {
+		
+		// BACKGROUND COLOR
+		g2.setColor(new Color(0,0,0));
+		g2.fillRect(0,0, gp.screenWidth, gp.screenHeight);
+
+		// FONT
+		g2.setFont(g2.getFont().deriveFont(Font.BOLD,96F));
+		// TEXT
+		String text = "Legend of Nine";
+
+		// LOCATION
+		int x = getXforCenteredText(text);
+		int y = gp.tileSize*3;
+
+		// TEXT SHADOW
+		g2.setColor(Color.gray);
+		g2.drawString(text, x+3, y+3);
+
+		// TEXT COLOR 
+		g2.setColor(Color.white);
+		g2.drawString(text, x, y);
+
+		// MOUSE SPRIT
+		x = gp.screenWidth/2 - (gp.tileSize); 
+		y += gp.tileSize*2;
+		g2.drawImage(gp.player.down1, x, y, gp.tileSize*2, gp.tileSize*2, null);
+
+		// MENU
+		g2.setFont(g2.getFont().deriveFont(Font.BOLD,48F));
+
+		text = "NEW GAME";
+		x = getXforCenteredText(text);
+		y += gp.tileSize*4;
+		g2.drawString(text, x, y);
+		if (commandNum == 0){
+			g2.drawString(">", x-gp.tileSize,y);
+		}
+
+		text = "QUIT";
+		x = getXforCenteredText(text);
+		y += gp.tileSize;
+		g2.drawString(text, x, y);
+		if (commandNum == 1){
+			g2.drawString(">", x-gp.tileSize,y);
+		}
+
+	}
 	/**
 	 * Display pause screen UI
 	 */
